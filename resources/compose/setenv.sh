ff54ebfa3adb5b6a510c4ad5b1686f82e1fab9aa
staillanibm <stephane.tailland@ibm.com>
2025-10-17 07:25:55 +0200

    First commit

---
+++ resources/compose/setenv.sh
@@ -0,0 +1,8 @@
+export COMPOSE_PATH_SEPARATOR=:
+
+export COMPOSE_FILE=./docker-compose.yml
+export COMPOSE_FILE=$COMPOSE_FILE:./postgres/docker-compose.yml
+export COMPOSE_FILE=$COMPOSE_FILE:./activetransfer-dev/docker-compose.yml
+export COMPOSE_FILE=$COMPOSE_FILE:./activetransfer-uat/docker-compose.yml
+#export COMPOSE_FILE=$COMPOSE_FILE:./haproxy/docker-compose.yml
+#export COMPOSE_FILE=$COMPOSE_FILE:./dbc/docker-compose.yml
